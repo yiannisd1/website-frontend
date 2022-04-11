6f397d7561d688b623b52387a8b217c5089434bd
SachiniWitharana <sachini.witha@gmail.com>
2022-04-11 14:14:59 +0530

    frontend-login

--- src/app/components/login/LoginComponent.jsx
+++ src/app/components/login/LoginComponent.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 // import Button from "react-bootstrap/Button";
-import {
-  Grid,
-  TextField,
-  Button
-} from '@material-ui/core';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 import "./LoginStyles.css"
 
 export default function LoginComponent() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  
   function validateForm() {
     return email.length > 0 && password.length > 0;
 }
@@ -23,32 +26,37 @@ export default function LoginComponent() {
 
 return (
   <div className="LoginComponent">
-    <div style={{ padding: 20 }}>
+    <div style={{ padding: 30 , color : 'white'}}>
     <Grid
       container
       spacing={1}
       direction={'column'}
       justify={'center'}
-      alignItems={'center'}
-      >
+      alignItems={'center'}>
       <h1>RedBack Login</h1>
       <Form onSubmit={handleSubmit}>
-
+      
       <Grid style={{ padding: 20 }} item xs={12}>
+        
         <Form.Group size="lg" controlId="email">
           <TextField label="Email"
             type="email"
+            variant="standard" 
+            sx={{ input: { color: 'white' } }} 
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             >
           </TextField>
         </Form.Group>
+        
         </Grid>
 
         <Grid style={{ padding: 20 }} item xs={12}>
         <Form.Group size="lg" controlId="password">
           <TextField label="Password"
             autofocus
+            variant="standard" 
+            sx={{ input: { color: 'white' } }} 
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -57,13 +65,13 @@ return (
         </Grid>
         <Grid style={{ padding: 20 }} item xs={12}>
         <Button 
-        style={{
-          borderRadius: 25,
-          backgroundColor: "#380e4a",
-          color: "#FFFF",
-          padding: "10px 20px",
-          fontSize: "15px"
-        }} 
+            style={{
+              borderRadius: 25,
+              backgroundColor: "#e87461",
+              color: "#FFFF",
+              padding: "10px 20px",
+              fontSize: "15px"
+            }} 
           fullWidth type="submit" disabled={!validateForm()}>
           Login
         </Button>
